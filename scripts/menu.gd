43633fa5ee37858d0dd3fbbac0c1abe1e1b408d9
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-12-20 00:52:19 +0300

    a big change
    is not completed but i hope i will XD

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -37,5 +37,6 @@ func _process(delta: float) -> void:
 		
 		
 	if Input.is_action_just_pressed("ui_select") and $sprite_menu.frame == 0:
-
-		get_tree().change_scene_to_file("res://scenes/smg_tool.tscn")
+		Across.swicher = 1
+		print (Across.swicher)
+		get_tree().change_scene_to_file("res://scenes/bord.tscn")
