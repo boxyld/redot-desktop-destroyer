b72db13d8ed38d42e885c491ef442ba77a688161
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-12-20 01:13:09 +0300

    swichable wopans
    fainally i did it :D

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -1,27 +1,29 @@
 extends Node2D
-var link := preload("res://scenes/smg.tscn")
-# Called when the node enters the scene tree for the first time.
-  # Adjust the range as needed
-func shoot():
-	var rng = RandomNumberGenerator.new()
-	var random_x_offset = rng.randf_range(-50, 50)  # Adjust the range as needed
-	var random_y_offset = rng.randf_range(-50, 50)
-	var yld = link.instantiate()
-	var positionyld = get_global_mouse_position()+ Vector2(random_x_offset,random_y_offset)
-	print(positionyld)
-	yld.position = get_global_mouse_position()+ Vector2(random_x_offset,random_y_offset)
-	add_child(yld)
 
+#the number will decide what wopan should it load
+var swichable = Across.swicher
+
+# the entire smg code
+var link := preload("res://scenes/smg.tscn")
+func shoot():
+	if swichable == 1:
+		var rng = RandomNumberGenerator.new()
+		var random_x_offset = rng.randf_range(-50, 50)  # Adjust the range as needed
+		var random_y_offset = rng.randf_range(-50, 50)
+		var yld = link.instantiate()
+		var positionyld = get_global_mouse_position()+ Vector2(random_x_offset,random_y_offset)
+		print(positionyld)
+		yld.position = get_global_mouse_position()+ Vector2(random_x_offset,random_y_offset)
+		add_child(yld)
 func _on_timer_timeout() -> void:
 	if Input.is_action_pressed("m1"):
 		shoot()
 		var smgs = get_node("AudioStreamPlayer2D")
 		smgs.play()
-
-
 func _process(delta: float) -> void:
 	
 	# add a routing to escape the app , temporary for now 
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://scenes/menu.tscn")
-		
+
+# there you can add another wopan
