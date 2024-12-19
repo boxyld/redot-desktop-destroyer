59586b87f423001030ff055c1717f392d0b7fb56
Box_yld <103340124+boxyld@users.noreply.github.com>
2024-12-20 01:22:08 +0300

    simple fixes

--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -16,14 +16,15 @@ func shoot():
 		yld.position = get_global_mouse_position()+ Vector2(random_x_offset,random_y_offset)
 		add_child(yld)
 func _on_timer_timeout() -> void:
-	if Input.is_action_pressed("m1"):
-		shoot()
-		var smgs = get_node("AudioStreamPlayer2D")
-		smgs.play()
+	if swichable ==1:
+		if Input.is_action_pressed("m1"):
+			shoot()
+			var smgs = get_node("AudioStreamPlayer2D")
+			smgs.play()
 func _process(delta: float) -> void:
 	
-	# add a routing to escape the app , temporary for now 
-	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	if swichable ==1:
+		if Input.is_action_just_pressed("ui_cancel"):
 
 # there you can add another wopan
